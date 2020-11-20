701f5654a769f217ada7bf5949403f7e6b212341
JoshGrant5 <joshgg@icloud.com>
2020-11-20 12:24:15 -0800

    imported VideoChat component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import VideoChat from './VideoChat';
 
 const App = () => {
   return (
@@ -8,13 +9,13 @@ const App = () => {
         <h1>Video Chat with Hooks</h1>
       </header>
       <main>
-        <p>VideoChat goes here.</p>
+        <VideoChat />
       </main>
       <footer>
         <p>
           Made with{' '}
           <span role="img" aria-label="React">
-            ⚛
+            ⚛️
           </span>{' '}
           by <a href="https://twitter.com/philnash">philnash</a>
         </p>
